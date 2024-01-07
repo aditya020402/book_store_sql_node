3459a1f2dc2e03579c789fea26781c8ec0a110db
aditya <aditya.as207@gmail.com>
2024-01-08 00:35:06 +0530

    completed the frontend and the backedn

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mysql from "mysql";
+import mysql from "mysql2";
 import cors from "cors";
 
 const app = express();
@@ -12,8 +12,16 @@ const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"aditya",
-    database:"books",
+    database:"test",
 });
+
+db.connect(function(err){
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+    console.log("connected with mysql database!!");
+})
 
 app.get("/",(req,res)=>{
     const q = "select * from books";
@@ -22,7 +30,7 @@ app.get("/",(req,res)=>{
             console.log(err);
             return res.json(err);
         }
-        return res.json(res);
+        return res.json(data);
     })
 })
 
@@ -30,8 +38,14 @@ app.get("/",(req,res)=>{
 app.post("/books",(req,res)=>{
     const q = "insert into books (`title`,`desc`,`price`,`cover`) values (?)";
     const values = [req.body.title,req.body.desc,req.body.price,req.body.cover];
+    console.log(values);
     db.query(q,[values],(err,data)=>{
-        if(err) return res.send(err);
+        if(err) {
+            console.log(err);
+            return res.send(err);
+        
+        }
+        console.log(data);
         return res.json(data);
     })
 })
@@ -58,6 +72,7 @@ app.put("/books/:id",(req,res)=>{
 })
 
 
-app.listen(8800,()=>{
-    console.log("connected to backend");
+const port = 8800;
+app.listen(port,()=>{
+    console.log(`backend working at ${port}`);
 })
